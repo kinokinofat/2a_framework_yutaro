b1877d01c7b70e7e206bb02ba9529b6fbd69f828
kosakuhiroto <hiro.kos_taka.2899_3@icloud.com>
2023-11-14 12:44:58 +0900

    aaaaa

---
+++ MyGitProject2c/src/main/java/controller/gitLoginController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class gitLoginController {
+
+    @RequestMapping(path = "/gitpralogin", method = RequestMethod.GET)
+    public String third() {
+        return "gitlogin";
+    }
+
+    @RequestMapping(path = "/gitpralogin", method = RequestMethod.POST)
+    public String third(String id, String pwd) {
+        System.out.println(id);
+        System.out.println(pwd);
+        if (id.equals("gitlogin") && pwd.equals("gitpw")) {
+            return "redirect:/gititemlist";
+        } else {
+            return "redirect:/gitlogin";
+        }
+    }
+
+   
+}
